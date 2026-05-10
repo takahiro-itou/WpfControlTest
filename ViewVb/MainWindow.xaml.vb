e19df08f653941d96e3b17472edd6c4a441fd38a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-10 19:47:58 +0900

    ユーザーコントロールのイベントハンドラを実装

--- ViewVb/MainWindow.xaml.vb
+++ ViewVb/MainWindow.xaml.vb
@@ -1,4 +1,26 @@
 ﻿
 Public Class MainWindow
 
+Private Sub RunSample(ByVal message As String)
+''--------------------------------------------------------------------
+''    サンプルコード
+''--------------------------------------------------------------------
+Dim outText As String
+
+    outText = $"入力した文字列は {message} です"
+    SampleControl1.OutputText = outText
+    MsgBox(outText, MsgBoxStyle.OkOnly)
+End Sub
+
+Private Sub SampleControl1_RunButtonClick(
+        ByVal sender As Object, ByVal e As EventArgs) _
+    Handles SampleControl1.RunButtonClick
+''--------------------------------------------------------------------
+''    イベントハンドラ
+''
+''    ユーザーコントロールの「実行ボタンクリック」
+''--------------------------------------------------------------------
+    RunSample(SampleControl1.InputText)
+End Sub
+
 End Class
