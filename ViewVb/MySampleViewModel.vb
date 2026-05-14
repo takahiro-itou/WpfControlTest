a7ef02e475eadd24819c24a9ed1608736e3306d8
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-14 12:47:38 +0900

    ファイルを新規作成

---
+++ ViewVb/MySampleViewModel.vb
@@ -0,0 +1,18 @@
+﻿
+Imports System.ComponentModel
+Imports System.Runtime.CompilerServices
+Imports System.Windows.Input
+
+Imports WpfControl.Sample
+
+Public Class SimpleCommand
+        Implements ICommand
+
+End Class
+
+
+Public Class MySampleViewModel
+        Implements INotifyPropertyChanged
+        Implements ISampleViewModel
+
+End Class
