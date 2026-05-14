636e18396c83268529dd6922717d20648132faf8
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-14 12:49:45 +0900

    クラス SimpleCommand にコンストラクタを実装

--- ViewVb/MySampleViewModel.vb
+++ ViewVb/MySampleViewModel.vb
@@ -8,6 +8,16 @@ Imports WpfControl.Sample
 Public Class SimpleCommand
         Implements ICommand
 
+Private ReadOnly m_execute As Action(Of Object)
+Private ReadOnly m_canExecute As Predicate(Of Object)
+
+Public Sub New(
+        execute As Action(Of OBject)
+        Optional canExecute As Predicate(Of Object) = Nothing)
+    Me.m_execute = execute
+    Me.m_canExecute = canExecute
+End Sub
+
 End Class
 
 
