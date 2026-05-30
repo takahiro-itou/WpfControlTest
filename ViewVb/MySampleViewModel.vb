5737f4cd0f9b1196f20125db4a2d05d940ab5b24
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-30 16:17:19 +0900

    リファクタリング

--- ViewVb/MySampleViewModel.vb
+++ ViewVb/MySampleViewModel.vb
@@ -9,41 +9,20 @@ Imports WpfControl.Sample
 Public Class MySampleViewModel
         Inherits WpfControl.Sample.SampleViewModel
 
-Private ReadOnly m_runButtonCommand As WpfControl.Common.SimpleCommand
 
-
-Public Sub New()
+Public Sub New(ByVal model As MySampleModel)
 ''--------------------------------------------------------------------
 ''    コンストラクタ
 ''--------------------------------------------------------------------
-
-    Me.m_runButtonCommand = New WpfControl.Common.SimpleCommand(
-        Sub(ByVal parameter As Object)
-            ExecuteRunButtonCommand()
-        End Sub,
-        Function(ByVal parameter As Object) As Boolean
-            Return  Me.InputText <> ""
-        End Function
-    )
-
+    MyBase.New(model)
 End Sub
-
-
-''--------------------------------------------------------------------
-''    RunButtonCommand  プロパティ
-''
-Public Overrides ReadOnly Property RunButtonCommand As ICommand
-    Get
-        Return  Me.m_runButtonCommand
-    End Get
-End Property
 
 
 Protected Overrides Sub raiseCanExecuteChanged()
 ''--------------------------------------------------------------------
 ''    CanExecuteChanged を発生させる
 ''--------------------------------------------------------------------
-    Me.m_runButtonCommand.RaiseCanExecuteChanged()
+
 End Sub
 
 
