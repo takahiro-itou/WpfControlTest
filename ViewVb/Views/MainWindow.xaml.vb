f55d562a6980b6af3d9ce9ece621d3ef7484fe1c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-06-28 11:09:18 +0900

    起動メニューのイベントハンドラを実装

--- ViewVb/Views/MainWindow.xaml.vb
+++ ViewVb/Views/MainWindow.xaml.vb
@@ -18,6 +18,15 @@ Private Sub mnuFileExit_Click(ByVal sender As Object, ByVal e As EventArgs)
     System.Windows.Application.Current.Shutdown()
 End Sub
 
+Private Sub mnuLaunchSample_Click(ByVal sender As Object, ByVal e As EventArgs)
+''--------------------------------------------------------------------
+''    メニュー「起動」－「サンプル」
+''--------------------------------------------------------------------
+    Dim frmSample As New SampleWindow()
+    frmSample.Show()
+End Sub
+
+
 End Class
 
 End Namespace
