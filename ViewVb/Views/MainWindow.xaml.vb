b5eaa31a8690a714877af158ebac37a8265f75fa
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-06-28 11:06:43 +0900

    コンパイルエラーを修正

--- ViewVb/Views/MainWindow.xaml.vb
+++ ViewVb/Views/MainWindow.xaml.vb
@@ -15,7 +15,7 @@ Private Sub mnuFileExit_Click(ByVal sender As Object, ByVal e As EventArgs)
 ''--------------------------------------------------------------------
 ''    メニュー「ファイル」－「終了」
 ''--------------------------------------------------------------------
-    System.Windows.Application.Current.Shutdown();
+    System.Windows.Application.Current.Shutdown()
 End Sub
 
 End Class
