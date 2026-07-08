64879e868e9b6d50584f2061055d949bbe488aa4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-08 21:54:20 +0900

    基底クラスのシグネチャ変更に対応

--- ViewVb/Models/TimeConsumingModel.vb
+++ ViewVb/Models/TimeConsumingModel.vb
@@ -4,4 +4,11 @@ Imports WpfControl.Utils
 Public Class TimeConsumingModel
         Inherits WpfControl.Utils.AbstractProgressModel
 
+Public Sub New(ByVal progress As IProgress(Of Integer))
+''--------------------------------------------------------------------
+''    コンストラクタ
+''--------------------------------------------------------------------
+    MyBase.New(progress)
+End Sub
+
 End Class
