556f52c5032cf0fa2442b611c762153092fd0496
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-10 22:31:23 +0900

    ビューのコマンドを実装

--- ViewVb/Views/ProgressWindow.xaml.vb
+++ ViewVb/Views/ProgressWindow.xaml.vb
@@ -4,6 +4,8 @@ Namespace Global.ViewVb.Views
 Public Class ProgressWindow
 
 Private m_model As TimeConsumingModel
+Private m_viewModel As WpfControl.Utils.ProgressViewModel
+
 
 Public Sub New()
 ''--------------------------------------------------------------------
@@ -11,7 +13,10 @@ Public Sub New()
 ''--------------------------------------------------------------------
     InitializeComponent()
     Me.m_model = New TimeConsumingModel()
-    Progress1.ViewModel = New WpfControl.Utils.ProgressViewModel(m_model)
+    Me.m_viewModel = New WpfControl.Utils.ProgressViewModel(m_model)
+
+    Me.DataContext = Me.m_viewModel
+    Progress1.ViewModel = Me.m_viewModel
 End Sub
 
 End Class
