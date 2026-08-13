cdc711485a6a2d19a246642567d273d72d4bc524
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-13 09:45:38 +0900

    リファクタリング

--- Rebuild.bat
+++ Rebuild.bat
@@ -1,4 +1,12 @@
 
-msbuild -restore -t:Rebuild ^
-    -p:Configuration="Release"  -p:Platform=x64     ^
-    WpfControlTest.sln
+set  solution=WpfControlTest
+set  config="Debug"
+
+
+msbuild  -restore  -t:Clean     ^
+    -p:Configuration=%config%   -p:Platform=x64     ^
+    "%solution%.sln"
+
+msbuild  -restore  -t:Rebuild   ^
+    -p:Configuration=%config%   -p:Platform=x64     ^
+    "%solution%.sln"
