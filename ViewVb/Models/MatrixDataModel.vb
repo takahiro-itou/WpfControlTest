4d23f1eb82e5c2e8e64ab0c494d89eca3c6f8832
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-04 20:37:09 +0900

    行列表示のテスト準備

---
+++ ViewVb/Models/MatrixDataModel.vb
@@ -0,0 +1,42 @@
+﻿
+Namespace Global.ViewVb
+
+Public Class MatrixDataModel
+
+Private m_data(12) As Integer
+
+
+Public Sub New()
+''--------------------------------------------------------------------
+''    コンストラクタ
+''--------------------------------------------------------------------
+Dim i As Integer
+
+    For i = 0 To 12 - 1
+        m_data(i) = i * i
+    Next i
+End Sub
+
+
+Public ReadOnly Property MatrixData As Integer()
+    Get
+        Return  Me.m_data
+    End Get
+End Property
+
+Public ReadOnly Property TotolColumns As Integer
+    Get
+        Return  4
+    End Get
+End Property
+
+Public ReadOnly Property TotalRows As Integer
+    Get
+        Return  3
+    End Get
+End Property
+
+
+End Class
+
+End Namespace
