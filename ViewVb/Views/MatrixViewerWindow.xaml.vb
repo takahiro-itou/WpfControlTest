5f10c3400ce1ea81fb90717b1613fcab3ef51c84
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-04 20:41:45 +0900

    コードビハインドを新規作成

---
+++ ViewVb/Views/MatrixViewerWindow.xaml.vb
@@ -0,0 +1,15 @@
+﻿
+Namespace Global.ViewVb.Views
+
+Public Class MatrixViewerWindow
+
+Public Sub New()
+''--------------------------------------------------------------------
+''    コンストラクタ
+''--------------------------------------------------------------------
+    InitializeComponent()
+End Sub
+
+End Class
+
+End Namespace
