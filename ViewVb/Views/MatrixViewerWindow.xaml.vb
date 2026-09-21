9969343d2c719fc4140e47ffea2b775ab7301d9d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-21 23:09:44 +0900

    メニューのイベントハンドラを実装

--- ViewVb/Views/MatrixViewerWindow.xaml.vb
+++ ViewVb/Views/MatrixViewerWindow.xaml.vb
@@ -10,6 +10,53 @@ Public Sub New()
     InitializeComponent()
 End Sub
 
+
+Private Sub mnuColExpand_Click(ByVal sender As Object, ByVal e As EventArgs) _
+    Handles mnuColExpand.Click
+''--------------------------------------------------------------------
+''    メニュー「列」－「拡大」
+''--------------------------------------------------------------------
+    Me.DataContext.DecaultCellWidth += 4.0
+End Sub
+
+
+Private Sub mnuColShrink_Click(ByVal sender As Object, ByVal e As EventArgs) _
+    Handles mnuColShrink.Click
+''--------------------------------------------------------------------
+''    メニュー「列」－「縮小」
+''--------------------------------------------------------------------
+    With Me.DataContext
+        .DefaultCellWidth -= 4.0
+        If ( .DefaultCellWidth <= 4.0 )  Then
+            .DefaultCellWidth = 4.0
+        End If
+    End With
+End Sub
+
+
+Private Sub mnuRowExpand_Click(ByVal sender As Object, ByVal e As EventArgs) _
+    Handles mnuRowExpand.Click
+''--------------------------------------------------------------------
+''    メニュー「行」－「拡大」
+''--------------------------------------------------------------------
+    Me.DataContext.DefaultCellHeight += 4.0
+End Sub
+
+
+Private Sub mnuRowShrink_Click(ByVal sender As Object, ByVal e As EventArgs) _
+    Handles mnuRowShrink.Click
+''--------------------------------------------------------------------
+''    メニュー「行」－「縮小」
+''--------------------------------------------------------------------
+    With Me.DataContext
+        .DefaultCellHeight -= 4.0
+        If ( .DefaultCellHeight <= 4.0 )  Then
+            .DefaultCellHeight = 4.0
+        End If
+    End With
+End Sub
+
+
 End Class
 
 End Namespace
